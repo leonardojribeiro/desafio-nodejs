c83b43fd8c503de9d4e11905c4091951d7f9b304
leonardojribeiro <leonardojr410@gmail.com>
2022-07-22 21:16:59 -0300

    refatoração para TypeScript e definição da estrutura inicial

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-//Voce deve rodar os testes usando:  npm test
-//Para testar a aplicação, rode: npm run dev
-
-//mais infos
-//https://github.com/ZijianHe/koa-router
-
-// todas as configuraçoes devem ser passadas via environment variables
-const PORT = process.env.PORT || 3000;
-
-const Koa = require('koa');
-const Router = require('koa-router');
+import Koa from 'koa';
+import Router from 'koa-router';
 
 const koa = new Koa();
-var router = new Router();
+const router = new Router();
 
 //rota simples pra testar se o servidor está online
 router.get('/', async (ctx) => {
@@ -21,14 +12,18 @@ router.get('/', async (ctx) => {
 //Uma rota de exemplo simples aqui.
 //As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
 router.get('/users', async (ctx) => {
-    ctx.status = 200;
-    ctx.body = {total:0, count: 0, rows:[]}
+  ctx.status = 200;
+  ctx.body = { total: 0, count: 0, rows: [] }
 });
 
 koa
   .use(router.routes())
   .use(router.allowedMethods());
 
-const server = koa.listen(PORT);
+const PORT = process.env.PORT || 3000;
+const server = koa.listen(
+  PORT,
+  () => console.log(`Servidor ouvindo a porta ${PORT}`),
+);
 
 module.exports = server;
